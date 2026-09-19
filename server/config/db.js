c9c03046ab073ae5bac31922824a363a921aa99d
Apurva1807 <24b01a12f4@svecw.edu.in>
2026-09-19 21:16:08 +0530

    Fix database configuration

--- server/config/db.js
+++ server/config/db.js
@@ -2,20 +2,21 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed");
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log("Database connection failed");
+    console.log(err);
+    return;
+  }
 
-    console.log("MySQL Connected Successfully");
+  console.log("MySQL Connected Successfully");
 });
 
 module.exports = db;
